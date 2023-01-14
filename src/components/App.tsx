d0ec23cc72fa3eb657798709e7e6c2df714575f3
Sankeeth Naguleswaran <n.sankeeth@gmail.com>
2023-01-14 17:57:20 -0500

    Implement current day weather section

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,20 +7,19 @@ interface Props {
 }
 
 interface State {
-  city: string
+  city: string,
+  weatherData: Array<any>
 }
 
 export default class App extends React.Component<Props, State> {
   cities = ['Ottawa', 'Moscow', 'Tokyo'];
-  weatherData = [];
 
   constructor(props: Props) {
     super(props);
     this.state = {
-      city: this.cities[0]
+      city: this.cities[0],
+      weatherData: []
     }
-
-    this.fetchWeatherData();
   }
 
   fetchWeatherData = (): void => {
@@ -30,18 +29,27 @@ export default class App extends React.Component<Props, State> {
     fetch(apiUrl)
       .then((res) => res.json())
       .then((data) => {
-        if (data?.list?.length > 0) this.weatherData = data.list;
+        if (data?.list?.length > 0) this.setState({ weatherData: data.list });
         else alert(`No weather found for "${this.state.city}"`);
       });
   }
 
   changeCity = (city: string): void => {
-    this.setState({ city: city });
-    this.fetchWeatherData()
+    this.setState({ city: city }, () => {
+      this.fetchWeatherData();
+    });
+  }
+
+  getCurrentWeatherData = () => this.state.weatherData[0];
+  getWeatherIconURL = (): string => `http://openweathermap.org/img/wn/${this.state.weatherData[0].weather[0]?.icon}@2x.png`;
+  kelvinToCelsius = (k: number) => (k - 273.15).toFixed(0);
+
+  componentDidMount() {
+    this.fetchWeatherData();
   }
 
   render() {
-    return (
+    return this.state.weatherData.length && (
       <div className="App main-container p-3">
         <div className='d-flex justify-content-around pt-3 pb-3'>
           {this.cities.map((city, index) => (
@@ -58,6 +66,22 @@ export default class App extends React.Component<Props, State> {
 
         <div className="p-0 bg-white shadow rounded weather-container overflow-hidden">
           <div className="container-fluid current-day">
+            <div className="pt-3 pb-3">
+              <div className='fs-3 fw-lighter'>Today</div>
+              <div className='w-50 p-3 m-auto d-flex'>
+                <div className='w-50 float-start text-end'>
+                  <img
+                    className='h-100 w-auto'
+                    src={this.getWeatherIconURL()}
+                    alt="weather status icon"
+                  />
+                </div>
+                <div className='w-50 float-end text-start'>
+                  <h1 className='fw-bold display-5'>{this.kelvinToCelsius(this.getCurrentWeatherData().main.temp)} &deg;</h1>
+                  <div className='fs-3 fw-lighter'>{this.getCurrentWeatherData().weather[0].main}</div>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="container-fluid p-0">
             <div className="other-days">
