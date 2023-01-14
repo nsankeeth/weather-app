35b5069551b93e38436f79a7f7f3cb3b70863fc9
Sankeeth Naguleswaran <n.sankeeth@gmail.com>
2023-01-14 14:25:46 -0500

    Convert functional component to class component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,26 +1,38 @@
 import React from 'react';
 import '../styles/App.less';
 
-function App() {
-  return (
-    <div className="App">
-      <div className='d-flex justify-content-around'>
-        <div className='button-container'>
+interface Props {
+}
+
+interface State {
+}
+
+export default class App extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className='d-flex justify-content-around'>
+          <div className='button-container'>
+          </div>
         </div>
-      </div>
 
 
-      <div className="container-lg p-0 bg-white shadow rounded weather-container overflow-hidden">
-        <div className="container-fluid current-day">
-        </div>
-        <div className="container-fluid p-0">
-          <div className="other-days">
-            <div></div>
+        <div className="container-lg p-0 bg-white shadow rounded weather-container overflow-hidden">
+          <div className="container-fluid current-day">
+          </div>
+          <div className="container-fluid p-0">
+            <div className="other-days">
+              <div></div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default App;
