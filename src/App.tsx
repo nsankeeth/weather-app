76b23047532702adb4d0995fe03ab1bf17692095
Sankeeth Naguleswaran <n.sankeeth@gmail.com>
2023-01-14 13:15:52 -0500

    Design main component

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,21 @@ import './App.less';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <div className='d-flex justify-content-around'>
+        <div className='button-container'>
+        </div>
+      </div>
+
+
+      <div className="container-lg p-0 bg-white shadow rounded weather-container overflow-hidden">
+        <div className="container-fluid current-day">
+        </div>
+        <div className="container-fluid p-0">
+          <div className="other-days">
+            <div></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
